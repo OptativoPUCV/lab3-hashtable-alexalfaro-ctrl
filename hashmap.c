88d28235295e3ee9c4e5741e534448485da1d8e3
Replit user <>
2023-09-09 00:53:13 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -98,7 +98,7 @@ void enlarge(HashMap * map) {
   map->buckets=(Pair**)malloc(map->capacity*sizeof(Pair*));
   map->size=0;
     
-  for(long i=0;map->capacity / 2 ;i++){ 
+  for(long i=0;i<map->capacity / 2 ;i++){ 
 
     if( aux[i] != NULL && aux[i]->key != NULL){ 
       insertMap(map,aux[i]->key,aux[i]->value);
