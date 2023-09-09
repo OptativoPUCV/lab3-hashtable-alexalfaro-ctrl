ac81411970e1870fe42316c4c99876ef177c6436
Replit user <>
2023-09-09 00:37:14 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -88,18 +88,21 @@ e - Inserte los elementos del arreglo *old_buckets* en el mapa (use la función 
 */
 void enlarge(HashMap * map) {
   enlarge_called = 1; //no borrar (testing purposes)
-  Pair **aux=map->buckets;
+  
+  
+  
   if(map!=NULL){
+    Pair **aux=map->buckets;
     map->capacity*=2;
-  map->buckets=(Pair**)malloc(map->capacity*sizeof(Pair*));
-  map->size=0;
-  for(long i=0;map->capacity;i++){
-    if( aux[i]!=NULL && aux[i]->key!=NULL){
-      insertMap(map,aux[i]->key,aux[i]->value);
+    map->buckets=(Pair**)malloc(map->capacity*sizeof(Pair*));
+    map->size=0;
+    for(long i=0;map->capacity;i++){
+      if( aux[i]!=NULL && aux[i]->key!=NULL){
+        insertMap(map,aux[i]->key,aux[i]->value);
       
+      }
     }
-  }
-  free(aux);
+    free(aux);
   }
   return;
 }
